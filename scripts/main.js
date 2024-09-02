539c904334f66108c36920af6b0b244e4eaca72a
Sundy <971213938@qq.com>
2024-09-02 19:17:57 +0800

    change

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function () {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/1.png') {
-        myImage.setAttribute('src', 'images/2.png');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
     } else {
-        myImage.setAttribute('src', 'images/1.png');
+        myImage.setAttribute('src', 'images/firefox-icon.png');
     }
 };
 
